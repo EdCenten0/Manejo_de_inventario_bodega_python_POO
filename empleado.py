3aaa8f6be8dab906aba212f74002a96e17ab3973
EdCenten0 <cchavarriacenteno8@gmail.com>
2022-12-07 21:13:56 -0600

    Se crearon los constructores

--- empleado.py
+++ empleado.py
@@ -4,3 +4,14 @@ class Empleado(Persona):
     puesto = str
     id_empleado = str
     salario = float
+
+    def __init__(self, puesto, id_empleado, salario):
+        self.puesto = puesto
+        self.id_empleado = id_empleado
+        self.salario = salario
+
+    def __init__(self):
+        self.puesto
+        self.id_empleado
+        self.salario
+        
