30c3f870ba2cb3c5c8442d5f04ab253703354d1b
EdCenten0 <cchavarriacenteno8@gmail.com>
2022-12-07 23:09:58 -0600

    Se creo una funcion especial para clientes

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import os
+import time
+
+def clientes():
+    os.system("clear")
+    print("Que desea hacer?: ")
+    
+    print("1. Agregar un cliente\n2. Leer los clientes\n3. Editar un cliente\n4. Eliminar un cliente\n5. Salir")
+    opc = int(input())
+    if(opc == 1):
+        print
+    elif(opc == 2):
+        print
+    elif(opc == 3):
+        print
+    elif(opc == 4):
+        print
+    elif(opc < 1 or opc > 5):
+        print("Digite una opcion valida")
+        time.sleep(3)
+        os.system("clear")
+        
+
+    
 
 def main():
     print("Menu")
@@ -8,9 +31,7 @@ def main():
         print("1. Clientes\n2. Empleados\n3. Productos\n4. Salir")
         opc = int(input())
         if(opc == 1):
-            os.system("clear")
-            print("Que desea hacer?: ")
-            print("1. Agregar un cliente\n2. Leer los clientes\n3. Editar un cliente\n 4. Eliminar un cliente")
+            clientes()
         elif():
             print()
 
